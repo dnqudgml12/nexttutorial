95dbe32b4778ee9112e12468fd6d898859c377bd
dnqudgml12 <21800454@handong.ac.kr>
2024-10-27 21:31:59 +0900

    ch11

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 export const experimental_ppr = true;
+//Partial Prerendering 이 가능하게 하였고 우리가 Suspense로 dynamic부분을 감싸면 자동으로 PPR이 되면서
+//next.js가 자동으로 dynamic 부분을 안다
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,42 @@
-'use client';
+'use client'; 
+//클라이언트 구성요소입니다. 즉, 이벤트 리스너와 후크를 사용할 수 있습니다.
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+/*
+useSearchParams
+Allows you to access the parameters of the current URL. For example, the search params for this URL /dashboard/invoices?page=1&query=pending would look like this: {page: '1', query: 'pending'}.
+*/
 export default function Search({ placeholder }: { placeholder: string }) {
+  
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+   /*
+   URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a.
+    */
+   params.set('page', '1');
+    console.log(term);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+    //deboucing으로 인해 300ms 후에 검색어를 업데이트합니다.
+  }, 300);
+
+//     //${pathname} is the current path, in your case, "/dashboard/invoices".
+//  As the user types into the search bar, params.toString() translates this input into a URL-friendly format.
+// replace(${pathname}?${params.toString()}) updates the URL with the user's search data.
+// For example, /dashboard/invoices?query=lee if the user searches for "Lee".
+//   
+  
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +45,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
